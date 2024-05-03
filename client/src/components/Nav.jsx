2430cd1780056e50c126d4081ce0b73ce0eca01d
oofytronic <oof@motualis.com>
2024-05-03 16:54:26 -0400

    set up easy auth for spotify

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import SpotifyAuth from './SpotifyAuth';
 
 function Nav({onChangeConsole}) {
 	const AUTH_URL = 'https://accounts.spotify.com/authorize?client_id=899a0e6b070d4b6eae06711c13eddd13&response_type=code&redirect_uri=http://localhost:5173&scope=ugc-image-upload%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-private%20playlist-modify-public%20user-library-modify%20user-library-read%20user-read-email%20user-read-private';
@@ -10,7 +11,7 @@ function Nav({onChangeConsole}) {
 			<ul>
 				<li><button onClick={() => onChangeConsole('playlists')}>Playlists</button></li>
 				<li><button onClick={() => onChangeConsole('search')}>Search</button></li>
-				<li><a href={AUTH_URL}>Login w/ Spotify</a></li>
+				<li><SpotifyAuth /></li>
 			</ul>
 		</nav>
 	)
---
+++ client/src/components/SpotifyAuth.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+
+const SpotifyAuth = () => {
+    useEffect(() => {
+        const hash = window.location.hash;
+        let token = window.localStorage.getItem('spotify_access_token');
+
+        if (!token && hash) {
+            token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1];
+            window.location.hash = ''; // Clear hash in URL
+            window.localStorage.setItem('spotify_access_token', token);
+        }
+
+        if (token) {
+            console.log('Token:', token);
+            // Perform API calls or redirect user
+        }
+
+        const error = new URLSearchParams(window.location.hash).get('error');
+        if (error) {
+            console.error('Error during authentication:', error);
+            // Handle errors here, such as showing a message to the user
+        }
+    }, []);
+
+    const handleLogin = () => {
+        const client_id = '899a0e6b070d4b6eae06711c13eddd13';
+        const redirect_uri = encodeURIComponent('http://localhost:5173');
+        const scopes = encodeURIComponent('ugc-image-upload playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-library-modify user-library-read user-read-email user-read-private'); // Scopes limit access for OAuth tokens
+        const responseType = 'token';
+
+        const authUrl = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=${responseType}&show_dialog=true`;
+        window.location = authUrl;
+    };
+
+    return (
+        <button onClick={handleLogin}>Log in with Spotify</button>
+    );
+};
+
+export default SpotifyAuth;
